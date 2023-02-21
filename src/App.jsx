bb40ea3d1943348a64c9f819b03ad568f991bcb1
Istvan Petrovan <petrovanistvan@gmail.com>
2023-02-21 17:15:58 +0100

    third iteration

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,38 @@ import { useState } from 'react'
 import './App.css'
 import contacts from "./contacts.json"
 
-let firstFive = contacts.slice(0, 5);
-console.log(firstFive);
+let actors = contacts.slice(0, 5);
+
+let allActors = contacts;
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [count, setCount] = useState(5);
+
+  function returnRandomActor() {
+    
+    let actorFound = false;
+    const randomActor = Math.floor(Math.random() * (allActors.length - 5)) + 5;
+    for (let i = 0; i < actors.length; i++) { 
+      if (actors[i].name === allActors[randomActor].name) {
+        actorFound = true;
+        break;
+      }
+    }
+    if (actorFound) {
+      returnRandomActor();
+    } else {
+      setCount(count + 1);
+      actors.push(allActors[randomActor]);
+    }
+  }
 
   return (
     <div className="App">
         <h1>Ironcontacts</h1>
+        <button className="add" onClick={returnRandomActor}>Add Random Contact</button>
+        <button className="sortByPopularity">Sort by popularity</button>
+        <button className="sortByName">Sort by name</button>
         <table>
           <thead>
             <tr>
@@ -19,10 +42,11 @@ function App() {
               <th>Popularity</th>
               <th>Won Oscar</th>
               <th>Won Emmy</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
-          {firstFive.map((actor) =>
+          {actors.map((actor) =>
             <tr>
               <td>
                 <img className="contactImage" src={actor.pictureUrl} alt="celeb" />
@@ -32,6 +56,7 @@ function App() {
               <td>{Math.round(actor.popularity * 100) / 100}</td>
               <td>{actor.wonOscar?"🏆":""}</td>
               <td>{actor.wonEmmy?"🏆":""}</td>
+              <td><button>Delete</button></td>
             </tr>
 
           )}
